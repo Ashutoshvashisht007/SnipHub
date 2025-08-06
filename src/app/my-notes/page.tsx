e9714a1632c43533682c4d48a3a8836ee92eab26
Ashutosh <ashuhina.123@gmail.com>
2025-08-06 18:27:51 +0530

    Sidebar done

--- src/app/my-notes/page.tsx
+++ src/app/my-notes/page.tsx
@@ -1,10 +1,14 @@
 import { UserButton } from '@clerk/nextjs'
 import React from 'react'
+import Sidebar from './components/Sidebar/page'
+
 
 export default function page(){
     return (
         <div>
-            <UserButton/>
+            <Sidebar />
+            <UserButton />
+
         </div>
     )
 }
